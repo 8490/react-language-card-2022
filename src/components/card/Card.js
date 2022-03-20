abb42ab3ba0f0e79be4736437edfa739a0ba72ca
8490 <ethan@clarusway.com>
2022-03-20 23:50:48 +0300

    add bar

---
+++ src/components/card/Card.js
@@ -0,0 +1,19 @@
+import "./Card.css";
+import { languages } from "../../helpers/data";
+import Item from "../item/Item";
+
+const Card = () => {
+  return (
+    <div className="cards-area-container">
+      <div className="bars"></div>
+      <h5 className="language-title">Languages</h5>
+      <div className="cards-container">
+        {languages.map((item, index) => {
+          return <Item card={item} key={index} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
