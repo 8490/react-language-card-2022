5c82d340db354ef251f075a63563674bba3cc37f
8490 <ethan@clarusway.com>
2022-03-20 23:29:39 +0300

    add App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/header/Header";
+import "./App.css";
+import Card from "./components/card/Card";
+
+function App() {
+  return (
+    <div className="main-container">
+      <Header />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
