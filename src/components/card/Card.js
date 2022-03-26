7a00cdefff88d0cd28f010b03d828503249034bb
8490 <ethan@clarusway.com>
2022-03-26 13:47:58 +0300

    update card.js

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -5,7 +5,7 @@ import Item from "../item/Item";
 const Card = () => {
   return (
     <div className="cards-area-container">
-      <div className="bars"></div>
+      {/* <div className="bars"></div> */}
       <h5 className="language-title">Languages</h5>
       <div className="cards-container">
         {languages.map((item, index) => {
